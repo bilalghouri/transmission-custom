25e2fb8a110b80f0ac859f22fbb60978a3a1567a
root <root@ns3028745.ip-51-255-65.eu>
2016-03-06 20:31:28 +0100

    Transmission bt modified for lse 2.84

---
+++ transmission-2.84+/libtransmission/version.h
@@ -0,0 +1,10 @@
+#define PEERID_PREFIX             "-TR284Z-"
+#define USERAGENT_PREFIX          "2.84+"
+#define SVN_REVISION              "14686"
+#define SVN_REVISION_NUM          14686
+#define SHORT_VERSION_STRING      "2.84+"
+#define LONG_VERSION_STRING       "2.84+ (14686)"
+#define VERSION_STRING_INFOPLIST  2.84+
+#define MAJOR_VERSION             2
+#define MINOR_VERSION             84
+#define TR_NIGHTLY_RELEASE        1
